7c843619ff10481d02ec1bd1624c974b0d43882c
Grant_Nathaniel_Brown <gnb225@nyu.edu>
2018-04-18 10:54:59 -0400

    have array of users that gets updated on join event and disconnect

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var axios = require('axios')
+
 // app.get('/', function (req, res) {
 //   res.sendFile(__dirname + '/index.html')
 // })
 
+var userArray = []
+
 io.on('connection', function (socket) {
-  identification = socket.id
-  socket.emit('user id', identification)
-  console.log(identification)
+
+  socket.on('user join game', (data) => {
+    userArray.push(data)
+    io.emit('user list', userArray)
+    console.log("ARRAY OF USERS: " + userArray)
+  })
+  // socket.on('connect', () => {
+  //   userArray.push(socket.id)
+  //   console.log(userArray)
+  
+  //   socket.emit('user id', userArray)
+
+  // } )
+
   socket.on('chat message', function (msg) {
     io.emit('chat message', msg)
   })
@@ -17,7 +31,7 @@ io.on('connection', function (socket) {
   socket.on('new question', function(question) {
 
     var randomQuestion = "http://jservice.io/api/random"
-    axios.get(randomQuestion).then((response)=> {
+    axios.get(randomQuestion).then((response) => {
       question = response.data[0]
       // 3
       io.emit('new question', question)
@@ -27,18 +41,25 @@ io.on('connection', function (socket) {
         console.log(error);
     });
   })
-  socket.on('correct', function(correctGuess) {
-    io.emit('correct', correctGuess)
+  socket.on('correct', (data) => {
+    var currentScores = data
+    io.emit('update scores', currentScores)
   })
   socket.on('incorrect', function(incorrectGuess) {
     io.emit('incorrect', incorrectGuess)
   })
-  // socket.on('disconnect', function () {
-  //   console.log('user disconnected')
-  // })
+  socket.on('disconnect', () => {
+    var index = userArray.indexOf(socket.id)
+    if (index !== -1) {
+      userArray.splice(index, 1)
+    }
+    io.emit('user list', userArray)
+    console.log(socket.id + " disconnected")
+    console.log("ARRAY OF USERS: " + userArray)
+  })
 })
 
-app.set('port', process.env.PORT || 3001)
+app.set('port', 3001)
 
 http.listen(app.get('port'), function () {
   console.log(`✅ PORT: ${app.get('port')} 🌟`)
